6dbfda3995882c3f2db47d1cf08ac6ef4a4ec515
oumarichard <richard.ouma@student.moringaschool.com>
2022-11-04 15:13:55 +0300

    Completed assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+      "#{self.actors.first.first_name} #{self.actors.first.last_name} - #{self.name}"
+  end
 end
